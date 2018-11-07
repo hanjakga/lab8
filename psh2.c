a7f8c13295382e8e5b5e9ab98c058327a058be1d
sondahum <willbedoc@naver.com>
2018-11-07 10:11:19 +0900

    Shell Command

---
+++ psh2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXARGS 20
+#define ARGLEN 100
+
+char* makestring(char*);
+void execcute(chqar*[]);
+
+int main()
+{
+	char* 
